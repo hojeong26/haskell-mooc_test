b36a6c3f1db3319426cdfd178ed86797105e9fb6
hojeong <rkdghwjd0926@jnu.ac.kr>
2023-10-11 18:57:02 +0900

    [Ex8]isZero함수를 Integer타입의 인자 전달하여 Bool타입의 값을 리턴할 수 있도록 정의해주고 패턴매칭을 사용하여 0이면 True를 0이 아닌 모든 수는 False를 반환할 수 있도록 한다.

--- exercises/Set1.hs
+++ exercises/Set1.hs
@@ -103,7 +103,9 @@ postagePrice gram = if gram > 500
 --
 -- Ps. remember, the type of booleans in haskell is Bool
 
-isZero = todo
+isZero :: Integer -> Bool
+isZero 0 = True
+isZero n = False
 
 ------------------------------------------------------------------------------
 -- Ex 9: implement using recursion a function sumTo such that
